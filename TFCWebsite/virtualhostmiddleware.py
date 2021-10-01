281e5863f38b58aaf5418b0f3346e695df48448e
Sambit009 <49282521+Sambit009@users.noreply.github.com>
2021-10-01 15:34:11 +0530

    Readme file updated

--- TFCWebsite/virtualhostmiddleware.py
+++ TFCWebsite/virtualhostmiddleware.py
@@ -2,17 +2,18 @@ virtual_hosts = {
     # Staging env
     "*.staging.forchange.in": "TFC.urls",
     "staging.forchange.in": "TFC.urls",
-    "staging.hackforchange.co.in": "HFCCore.urls",
+    "staging.toolsforchange.co.in": "TFC.urls",
+    
 
     # Production
     "*.forchange.in": "TFC.urls",
     "www.forchange.in": "TFC.urls",
-    "www.hackforchange.co.in": "HFCCore.urls",
+    
 
     # Local Dev
     "*.dev.forchange.in:8000": "TFC.urls",
     "dev.forchange.in:8000": "TFC.urls",
-    "dev.hackforchange.co.in:8000": "HFCCore.urls",
+    
     
 }
 
